1b2985a008a5b8badfcdbab2ffcbe160dffc7bd0
shivanshcode <70197059+shivanshcode@users.noreply.github.com>
2025-04-16 09:29:32 +0545

    Update embed.py

--- models/embed.py
+++ models/embed.py
@@ -97,7 +97,7 @@ class DataEmbedding(nn.Module):
     def __init__(self, c_in, d_model, embed_type='fixed', freq='h', dropout=0.1):
         super(DataEmbedding, self).__init__()
 
-        self.value_embedding = TokenEmbedding(c_in=c_in*8, d_model=d_model)
+        self.value_embedding = TokenEmbedding(c_in=c_in*4, d_model=d_model)
         self.position_embedding = PositionalEmbedding(d_model=d_model)
         self.temporal_embedding = TemporalEmbedding(d_model=d_model, embed_type=embed_type, freq=freq) if embed_type!='timeF' else TimeFeatureEmbedding(d_model=d_model, embed_type=embed_type, freq=freq)
 
@@ -105,7 +105,7 @@ class DataEmbedding(nn.Module):
 
     def forward(self, x, x_mark):
         # Add 1
-        x = x.reshape(x.shape[0], -1, int(x.shape[2]*8))
+        x = x.reshape(x.shape[0], -1, int(x.shape[2]*4))
         #
         x = self.value_embedding(x) + self.position_embedding(x) #+ self.temporal_embedding(x_mark)
         
